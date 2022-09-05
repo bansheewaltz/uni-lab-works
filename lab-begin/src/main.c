c1d292aa2f434f2ca6176db72fae232430aad580
Anton Grevtsov <grevtsov.apn@gmail.com>
2022-09-05 22:04:03 +0700

    test

--- lab-begin/src/main.c
+++ lab-begin/src/main.c
@@ -1,6 +1,20 @@
+#include <stdio.h>
 #include <stdlib.h>
 
+<<<<<<< Updated upstream
 int main(void) {
   int a = 0;
   return EXIT_SUCCESS;
+=======
+int int_read(int *n) { return scanf("%d", n); }
+int in_bounds(int *n, int a, int b) { return a <= *n && *n <= b; }
+int read_number_of_elements(int *n) {
+  return int_read(n) && in_bounds(n, 1, 10);
+}
+
+int main(void) {
+  int number_of_elements = 0;
+  if (!read_number_of_elements(&number_of_elements)) printf("bad input");
+  return 0;
+>>>>>>> Stashed changes
 }
