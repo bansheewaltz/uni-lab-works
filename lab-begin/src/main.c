b053e9cfcd74ec71651b893345ef891e7d7a200b
Antoine <mark@userui-MacBookAir.local>
2022-09-05 22:05:00 +0700

    test

--- lab-begin/src/main.c
+++ lab-begin/src/main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< Updated upstream
-int main(void) {
-  int a = 0;
-  return EXIT_SUCCESS;
-=======
 int int_read(int *n) { return scanf("%d", n); }
 int in_bounds(int *n, int a, int b) { return a <= *n && *n <= b; }
 int read_number_of_elements(int *n) {
@@ -16,5 +11,4 @@ int main(void) {
   int number_of_elements = 0;
   if (!read_number_of_elements(&number_of_elements)) printf("bad input");
   return 0;
->>>>>>> Stashed changes
 }
