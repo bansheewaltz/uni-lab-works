e72c2fe420a25719d383abd515d53bf7c60994c2
Anton Grevtsov <grevtsov.apn@gmail.com>
2022-09-12 15:57:37 +0700

    check

--- lab-begin/src/main.c
+++ lab-begin/src/main.c
@@ -3,88 +3,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool char_read(char *c) { return scanf("%c", c); }
+bool char_read(char *c) {
+    return scanf("%c", c);
+}
 bool char_is_digit(char input) {
-  return ('1' <= input && input <= '9') || input == '-';
+    return ('1' <= input && input <= '9') || input == '-';
 }
-bool int_read(long long *n) { return scanf("%lld", n); }
+bool int_read(long long *n) {
+    return scanf("%lld", n);
+}
 bool clear_input_buffer(void) {
-  int c;
+    int c;
 
-  while ((c = getchar()) != '\n')
-    ;
-  return true;
+    while ((c = getchar()) != '\n') {
+        ;
+    }
+    return true;
 }
-bool in_bounds(long long *n, int a, int b) { return a <= *n && *n <= b; }
-bool underflow_check(int current_n, int limit) { return current_n < limit; }
-bool overflow_check(int current_n, int limit) { return current_n > limit; }
+bool in_bounds(long long *n, int a, int b) {
+    return a <= *n && *n <= b;
+}
+bool underflow_check(int current_n, int limit) {
+    return current_n < limit;
+}
+bool overflow_check(int current_n, int limit) {
+    return current_n > limit;
+}
 bool sum_overflow_check(int a, int b) {
-  return ((a > 0 && b > INT_MAX - a) || (a <= 0 && b < INT_MIN - a));
+    return ((a > 0 && b > INT_MAX - a) || (a <= 0 && b < INT_MIN - a));
 }
-void print_error(void) { printf("bad input"); }
+void print_error(void) {
+    printf("bad input");
+}
 bool read_number_of_elements(long long *n) {
-  return int_read(n) && in_bounds(n, 1, 10) && clear_input_buffer();
+    return int_read(n) && in_bounds(n, 1, 10) && clear_input_buffer();
 }
-void read_elements(int *array);
 bool element_is_good(char input) {
-  return (char_is_digit(input) || input == ' ');
+    return (char_is_digit(input) || input == ' ');
 }
-// void array_print(int *array, int array_size) {
-//   for (int i = 0; i < array_size; ++i) {
-//     printf("%d%s", array[i], i == array_size - 1 ? "" : " ");
-//   }
-// }
 
 int main(void) {
-  long long number_of_elements = 0;
-  bool error = false;
-  int *array = NULL;
-  int sum = 0;
-  bool error_outputed = false;
+    long long number_of_elements = 0;
+    bool error = false;
+    int *array = NULL;
+    int sum = 0;
+    bool error_outputed = false;
 
-  if (read_number_of_elements(&number_of_elements)) {
-    array = malloc(sizeof(int) * number_of_elements);
-    char input = 0;
-    int i = 0;
+    if (read_number_of_elements(&number_of_elements)) {
+        array = malloc(sizeof(int) * number_of_elements);
+        char input = 0;
+        int i = 0;
 
-    while (char_read(&input) && element_is_good(input) && !error) {
-      if (char_is_digit(input)) {
-        long long number = 0;
-        ungetc(input, stdin);
+        while (char_read(&input) && element_is_good(input) && !error) {
+            if (char_is_digit(input)) {
+                long long number = 0;
+                ungetc(input, stdin);
 
-        if (int_read(&number) && in_bounds(&number, INT_MIN, INT_MAX)) {
-          array[i++] = number;
+                if (int_read(&number) && in_bounds(&number, INT_MIN, INT_MAX)) {
+                    array[i++] = number;
 
-          if (sum_overflow_check(sum, number)) {
-            error = true;
-            printf("overflow");
-            error_outputed = true;
-          } else {
-            sum += number;
-          }
+                    if (sum_overflow_check(sum, number)) {
+                        error = true;
+                        printf("overflow");
+                        error_outputed = true;
+                    } else {
+                        sum += number;
+                    }
+                } else {
+                    error = true;
+                }
 
-        } else
-          error = true;
-
-        if (overflow_check(i, number_of_elements)) {
-          error = true;
+                if (overflow_check(i, number_of_elements)) {
+                    error = true;
+                }
+            }
         }
-      }
+
+        if (underflow_check(i, number_of_elements)) {
+            error = true;
+        }
+    } else {
+        error = true;
     }
 
-    if (underflow_check(i, number_of_elements)) error = true;
-  } else {
-    error = true;
-  }
+    if (!error_outputed) {
+        if (error) {
+            print_error();
+        } else {
+            printf("%d", sum);
+        }
+    }
 
-  if (!error_outputed) {
-    if (error)
-      print_error();
-    else
-      printf("%d", sum);
-  }
-
-  free(array);
-  array = 0;
-  return 0;
+    free(array);
+    array = 0;
+    return 0;
 }
