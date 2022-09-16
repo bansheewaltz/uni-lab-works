71237cbc572144904e5546ac2020a15941fa489b
mark <mark@machine.local>
2022-09-16 22:38:45 +0700

    check

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -1,11 +1,15 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MIN_BASE 1
+
+#define MIN_BASE 2
 #define MAX_BASE 16
 #define BASE_LIMITS MIN_BASE, MAX_BASE
-#define MAX_BUFFER 13
+#define MAX_BUFFER (13 + 1)
+#define EXIT_SUCCESS 0
+// #define EXIT_FAILURE 0
 
 bool int_in_bounds(long long n, int left_bound, int right_bound) {
     return left_bound <= n && n <= right_bound;
@@ -46,11 +50,25 @@ bool digits_check(const char *input, int base) {
             return false;
         }
     }
+
     return true;
 }
 void clear_input_buffer(void) {
     while (getchar() != '\n') {
     }
+}
+int binary_power(int number, int power) {
+    int result = 1;
+
+    while (power) {
+        if (power & 1) {
+            result *= number;
+        }
+        number *= number;
+        power >>= 1;
+    }
+
+    return result;
 }
 int main(void) {
     // char input_buffer[MAX_BUFFER];
@@ -68,37 +86,50 @@ int main(void) {
 
     if (!(scanf("%d %d", &b1, &b2) == 2 && int_in_bounds(b1, BASE_LIMITS) && int_in_bounds(b2, BASE_LIMITS))) {
         print_input_error();
-        return 0;
+        return EXIT_FAILURE;
     }
 
-    // fflush(stdin);
     clear_input_buffer();
     char input_buffer[MAX_BUFFER];
-    if (fgets(input_buffer, MAX_BUFFER, stdin) == NULL) {
+    if (fgets(input_buffer, MAX_BUFFER, stdin) == NULL || !digits_check(input_buffer, b1)) {
         print_input_error();
-        return 0;
+        return EXIT_FAILURE;
     }
 
-    if (!digits_check(input_buffer, b1)) {
-        print_input_error();
-        return 0;
-    }
     if (b1 == b2) {
-        input_buffer[strlen(input_buffer) - 1] = '\0';
+        // input_buffer[strlen(input_buffer) - 1] = '\0';
+        input_buffer[strcspn(input_buffer, "\n")] = '\0';
         printf("%s", input_buffer);
-        return 0;
+        return EXIT_SUCCESS;
     }
 
     char *cursor = input_buffer;
     int whole_part = strtol(cursor, &cursor, b1);
-    int fractional_part = 0;
+    // int fractional_part = 0;
+    float f = 0;
+    char result[13];
     if (*cursor == '.') {
-        fractional_part = strtol(cursor + 1, &cursor, b2);
+        cursor++;
+        char n[2] = {0};
+        for (int i = 0; cursor[i]; i++) {  // iterating the fraction string
+            n[0] = cursor[i];
+            // f += strtol(n, 0, b1) * binary_power(b1, -i - 1);  // converting the fraction part
+            f += strtol(n, 0, b1) * pow(b1, -(i + 1));  // converting the fraction part
+        }
+        int j = 0;
+        while (f > 0)  // do multiply and get the int part until number is zero
+        {
+            f *= b2;                    // do multiply by base and store it in number.
+            result[j++] = (int)f + 48;  // store the int part.
+            f -= (int)f;                // remove the int part.
+        }
     }
     char part1[15];
-    char part2[15];
+    // char part2[15];
+    // printf("%d %s\n", whole_part, result);
+    // printf("%d %f\n", whole_part, f);
     decimal_to_base(part1, b2, whole_part);
-    decimal_to_base(part2, b2, fractional_part);
-    printf("%s.%s", part1, part2);
-    // printf("%d.%d", whole_part, fractional_part);
+    // decimal_to_base(part2, b2, fractional_part);
+    printf("%s.%s", part1, result);
+    return EXIT_SUCCESS;
 }
