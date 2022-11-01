b0e5a05d8d2e0961f61381ab17e777524f9c84db
Anton Grevtsov <grevtsov.apn@gmail.com>
2022-11-01 21:29:23 +0700

    check

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -9,12 +9,12 @@
 #define TEXT_BUFFER_SIZE 500  // min = PATTERN_INPUT_LIMIT * 2 because of buffer updates by halfs
 typedef unsigned char uchar;
 
-int update_buffer(char* buffer, size_t window_pos) {
+int update_buffer(char buffer[], size_t window_pos) {
     char* buffer_half = window_pos ? buffer + TEXT_BUFFER_SIZE / 2 : buffer;
     return fread(buffer_half, sizeof(char), TEXT_BUFFER_SIZE / 2, stdin);
 }
 
-void search_substring(const char* pattern, int len_pattern, char* text, int len_text_buf) {
+void search_substring(const char pattern[], int len_pattern, char text[], int len_text_buf) {
     int hash_pattern = 0;
     int hash_window = 0;  // window as in 'sliding window technique'
     int pow_max = 1;
@@ -51,7 +51,7 @@ int main(void) {
 
     char pattern[PATTERN_SIZE] = "";
     if (!fgets(pattern, PATTERN_SIZE, stdin) || !strchr(pattern, '\n')) {  // emptiness and overflow check
-        return EXIT_SUCCESS;  // but actually ERROR
+        return EXIT_SUCCESS;  // but actually FAILURE
     }
     int len_pattern = strlen(pattern) - 1;  // to ignore '\n' char
 
@@ -59,7 +59,7 @@ int main(void) {
     int len_text_buf = fread(text, sizeof(char), TEXT_BUFFER_SIZE, stdin);
     if (len_pattern > len_text_buf || !len_pattern) {  // only '\n' pattern check
         len_text_buf ? 0 : printf("0");  // check if text is empty
-        return EXIT_SUCCESS;  // but actually ERROR
+        return EXIT_SUCCESS;  // but actually FAILURE
     }
 
     search_substring(pattern, len_pattern, text, len_text_buf);
