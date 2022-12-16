22225a38bb04290a3908f1a22fe283e53de3d98c
mark <mark@machine.local>
2022-12-16 19:00:41 +0700

    main decompose

--- lab4/src/main.c
+++ lab4/src/main.c
@@ -1,7 +1,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define INPUT_LIMIT 1000
+#define INPUT_MAX_LEN (INPUT_LIMIT + 1)  // because of '\n'
 
 typedef struct stack {
     int items[INPUT_LIMIT];
@@ -14,6 +16,10 @@ bool is_operator(char c) {
 
 bool is_digit(char c) {
     return c >= '0' && c <= '9';
+}
+
+bool is_unary_minus(char *char_p) {
+    return *char_p == '-' && is_digit(*++char_p);
 }
 
 bool symbol_validation(char c) {
@@ -62,7 +68,7 @@ int stack_peek(stack *st) {
     return st->items[st->size - 1];
 }
 
-void convert_to_RPN(char *input_string, char *RPN_string) {
+void infix_to_postfix(char *input_string, char *postfix_string) {
     stack st_operators;
     stack_init(&st_operators);
     int i = 0;
@@ -71,13 +77,13 @@ void convert_to_RPN(char *input_string, char *RPN_string) {
     while (input_string[i] != '\0') {
         if (is_digit(input_string[i])) {
             while (!is_operator(input_string[i])) {
-                RPN_string[RPN_str_len] = input_string[i];
+                postfix_string[RPN_str_len] = input_string[i];
                 RPN_str_len++;
                 i++;
                 if (input_string[i] == '\0')
                     break;
             }
-            RPN_string[RPN_str_len] = ' ';
+            postfix_string[RPN_str_len] = ' ';
             RPN_str_len++;
         }
 
@@ -91,8 +97,8 @@ void convert_to_RPN(char *input_string, char *RPN_string) {
                 }
                 char tmp = (char)stack_pop(&st_operators);
                 while (tmp != '(') {
-                    RPN_string[RPN_str_len] = tmp;
-                    RPN_string[RPN_str_len + 1] = ' ';
+                    postfix_string[RPN_str_len] = tmp;
+                    postfix_string[RPN_str_len + 1] = ' ';
                     RPN_str_len += 2;
                     tmp = (char)stack_pop(&st_operators);
                 }
@@ -101,8 +107,9 @@ void convert_to_RPN(char *input_string, char *RPN_string) {
                 if (st_operators.size > 0 &&
                     get_op_precedence((char)stack_peek(&st_operators)) >=
                         get_op_precedence(input_string[i])) {
-                    RPN_string[RPN_str_len] = (char)stack_pop(&st_operators);
-                    RPN_string[RPN_str_len + 1] = ' ';
+                    postfix_string[RPN_str_len] =
+                        (char)stack_pop(&st_operators);
+                    postfix_string[RPN_str_len + 1] = ' ';
                     RPN_str_len += 2;
                 }
                 stack_push(&st_operators, (int)input_string[i]);
@@ -112,11 +119,11 @@ void convert_to_RPN(char *input_string, char *RPN_string) {
     }
 
     while (st_operators.size > 0) {
-        RPN_string[RPN_str_len] = (char)stack_pop(&st_operators);
-        RPN_string[RPN_str_len + 1] = ' ';
+        postfix_string[RPN_str_len] = (char)stack_pop(&st_operators);
+        postfix_string[RPN_str_len + 1] = ' ';
         RPN_str_len += 2;
     }
-    RPN_string[RPN_str_len] = '\0';
+    postfix_string[RPN_str_len] = '\0';
 }
 
 int apply_op(int a, int b, char op) {
@@ -133,42 +140,44 @@ int apply_op(int a, int b, char op) {
     }
 }
 
-int eval_expr(char *RPN_string) {
-    stack operands;
-    stack_init(&operands);
+int eval_expr(char *postfix_string) {
+    stack st_operands;
+    stack_init(&st_operands);
 
-    int i = 0;
-    while (RPN_string[i] != '\0') {
-        if (RPN_string[i] == ' ') {
-            ++i;
+    char *char_p = postfix_string;
+    while (*char_p != '\0') {
+        if (*char_p == ' ') {
+            ++char_p;
             continue;
         }
-        if (is_digit(RPN_string[i])) {
-            int number = 0;
-            while (is_digit(RPN_string[i])) {
-                number *= 10;
-                number += (int)(RPN_string[i] - '0');
-                ++i;
-            }
-            stack_push(&operands, number);
-            ++i;
+        if (is_digit(*char_p) || is_unary_minus(char_p)) {
+            int number = strtol(char_p, &char_p, 10);
+
+            stack_push(&st_operands, number);
             continue;
         }
-        if (is_operator(RPN_string[i])) {
-            char op = RPN_string[i];
-            int b = (int)stack_pop(&operands);
-            int a = (int)stack_pop(&operands);
+        if (is_operator(*char_p)) {
+            char op = *char_p;
+            int b = (int)stack_pop(&st_operands);
+            int a = (int)stack_pop(&st_operands);
             int res = apply_op(a, b, op);
 
-            stack_push(&operands, res);
-            ++i;
+            stack_push(&st_operands, res);
+            ++char_p;
         }
     }
 
-    return stack_pop(&operands);
+    return stack_pop(&st_operands);
 }
 
-void input_validation(char string[]) {
+void input_read(char *string) {
+    if (fgets(string, INPUT_MAX_LEN, stdin) == NULL) {
+        syntax_error();
+    }
+    string[strcspn(string, "\n")] = '\0';
+}
+
+void input_validate(char string[]) {
     for (int i = 0; string[i] != '\0'; ++i) {
         if (!symbol_validation(string[i])) {
             syntax_error();
@@ -177,15 +186,14 @@ void input_validation(char string[]) {
 }
 
 int main(void) {
-    char input_string[INPUT_LIMIT];
-    if (!scanf("%1001[^\n]s", input_string)) {
-        syntax_error();
-    }
-    input_validation(input_string);
-    char RPN_string[INPUT_LIMIT];
+    char input_string[INPUT_MAX_LEN] = "";
 
-    convert_to_RPN(input_string, RPN_string);
-    printf("%d", eval_expr(RPN_string));
+    input_read(input_string);
+    input_validate(input_string);
+    char postfix_string[INPUT_LIMIT] = "";
+
+    infix_to_postfix(input_string, postfix_string);
+    printf("%d", eval_expr(postfix_string));
 
     return EXIT_SUCCESS;
 }
