f7a2fcc8a75f8c50639703d63b322d5449333efa
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-04-30 02:46:34 +0700

    Revert "unsuccessful attemp to create mapping array"
    This reverts commit 170b0ab4ae97a6698d4433233002ba4d577331fe.

--- lab11/src/sort.c
+++ lab11/src/sort.c
@@ -1,23 +1,20 @@
 #include "sort.h"
 
 #include <stdbool.h>
-#include <stdlib.h>
 
-#include "utils.h"
-
-static void array_int_counting_sort(int mapping[], int arr[], int size,
-                                    int place, bool order) {
+static void counting_sort_int_int(int arr[], int arr_by_which[], int size,
+                                  int place, bool order) {
   int max_digit = COUNTING_SORT_BASE - 1;
   int count[COUNTING_SORT_BASE] = {0};
-  // int mapping_output[size];
   int arr_output[size];
+  int arr_by_output[size];
 
   // Calculate count of elements
   for (int i = 0; i < size; ++i) {
-    count[(arr[i] / place) % COUNTING_SORT_BASE]++;
+    count[(arr_by_which[i] / place) % COUNTING_SORT_BASE]++;
   }
   // Calculate cumulative count
-  if (order == DESCENDING) {
+  if (order) {
     for (int i = max_digit; i > 0; --i) {
       count[i - 1] += count[i];
     }
@@ -28,35 +25,29 @@ static void array_int_counting_sort(int mapping[], int arr[], int size,
   }
   // Place the elements in sorted order
   for (int i = size - 1; i >= 0; --i) {
-    // mapping_output[--count[(arr[i] / place) % COUNTING_SORT_BASE]] =
-    // mapping[i];
-    arr_output[--count[(arr[i] / place) % COUNTING_SORT_BASE]] = arr[i];
-    mapping[i] = count[(arr[i] / place) % COUNTING_SORT_BASE];
+    arr_output[--count[(arr_by_which[i] / place) % COUNTING_SORT_BASE]] =
+        arr[i];
+    arr_by_output[count[(arr_by_which[i] / place) % COUNTING_SORT_BASE]] =
+        arr_by_which[i];
   }
-  // Copy the sorted elements into original arr
+  // Copy the sorted elements into original arr_by_which
   for (int i = 0; i < size; ++i) {
-    // mapping[i] = mapping_output[i];
     arr[i] = arr_output[i];
+    arr_by_which[i] = arr_by_output[i];
   }
-  array_int_print(arr, size, 3);
-  array_int_print(mapping, size, 3);
 }
 
-int *array_int_radix_sort(int arr[], int arr_length, bool order) {
-  int max = arr[0];
+void radix_sort_int_int(int arr[], int arr_by_which[], int arr_length,
+                        bool order) {
+  int max = arr_by_which[0];
 
   for (int i = 1; i < arr_length; ++i) {
-    if (arr[i] > max) {
-      max = arr[i];
+    if (arr_by_which[i] > max) {
+      max = arr_by_which[i];
     }
   }
 
-  int *mapping = (int *)malloc(sizeof(int) * arr_length);
-  if_fail(mapping == NULL, __FILE__, __LINE__);
-
   for (int place = 1; max / place > 0; place *= COUNTING_SORT_BASE) {
-    array_int_counting_sort(mapping, arr, arr_length, place, order);
+    counting_sort_int_int(arr, arr_by_which, arr_length, place, order);
   }
-
-  return mapping;
 }
