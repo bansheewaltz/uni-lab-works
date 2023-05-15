f787a9678b215d8147d071c8c5234594b627ec1e
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-16 00:16:47 +0700

    decomposition: custom types added

--- lab7/src/headers/input.h
+++ lab7/src/headers/input.h
@@ -3,8 +3,8 @@
 
 bool scan_validate_vertices_count(int *vertices_count);
 bool scan_validate_edges_count(int *edges_count, int vertices_count);
-bool scan_validate_parameters(int *vertices_count, int *edges_count);
+bool graph_scan_validate_parameters(int *vertices_count, int *edges_count);
 bool validate_edge(int src, int dst, int vertices_count);
-bool scan_edges(bool *graph_array, int vertices_count, int edges_count);
+bool graph_scan_validate_edges(Graph *graph);
 
 #endif  // INPUT_H
