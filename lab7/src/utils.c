9ec08f161c48d4d56bf72807dcdb38c1ab520367
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-15 16:18:44 +0700

    all functional tests are passed

--- lab7/src/utils.c
+++ lab7/src/utils.c
@@ -5,20 +5,32 @@
 
 #include "main.h"
 
-void *safe_malloc(size_t size, char *file, uint line) {
+void print_error(char *file, int line, char *error_msg) {
+  fprintf(stderr, "Internal Error: %s:%d: %s\n", file, line, error_msg);
+}
+
+void *safe_malloc(size_t size, char *file, int line) {
   void *pointer = malloc(size);
   if (pointer == NULL) {
-    fprintf(stdout, "Internal Error: %s:%d: %s\n", file, line, strerror(errno));
+    print_error(file, line, strerror(errno));
     exit(EXIT_FAILURE);
   }
   return pointer;
 }
 
-void *safe_calloc(size_t n, size_t size, char *file, uint line) {
+void *safe_calloc(size_t n, size_t size, char *file, int line) {
   void *pointer = calloc(n, size);
   if (pointer == NULL) {
-    fprintf(stdout, "Internal Error: %s:%d: %s\n", file, line, strerror(errno));
+    print_error(file, line, strerror(errno));
     exit(EXIT_FAILURE);
   }
   return pointer;
+}
+
+void stack_array_print(int *stack_array, int length) {
+  for (int i = length; i >= 0; --i) {
+    if (stack_array[i] != 0) {
+      printf("%d%s", stack_array[i], i == 0 ? "" : " ");
+    }
+  }
 }
