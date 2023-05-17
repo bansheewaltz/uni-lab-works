2cc9f8edd3c7c781380b112893d2db8aad950380
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-17 11:22:14 +0700

    utils.c: commented out exits removed

--- lab7/src/utils.c
+++ lab7/src/utils.c
@@ -20,7 +20,6 @@ void *safe_malloc(size_t size, char *file, int line) {
   void *pointer = malloc(size);
   if (pointer == NULL) {
     print_error(file, line, strerror(errno));
-    // exit(EXIT_FAILURE);
   }
   return pointer;
 }
@@ -29,7 +28,6 @@ void *safe_calloc(size_t n, size_t size, char *file, int line) {
   void *pointer = calloc(n, size);
   if (pointer == NULL) {
     print_error(file, line, strerror(errno));
-    // exit(EXIT_FAILURE);
   }
   return pointer;
 }
