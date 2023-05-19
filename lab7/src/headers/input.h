420d13fe658916fbdb8f08c43c03e87d39f98267
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-20 01:36:19 +0700

    main.c: refactoring

--- lab7/src/headers/input.h
+++ lab7/src/headers/input.h
@@ -7,8 +7,8 @@
 
 bool scan_validate_vertices_count(int *vertices_count);
 bool scan_validate_edges_count(int *edges_count, int vertices_count);
-bool graph_scan_validate_parameters(int *vertices_count, int *edges_count);
+bool scan_validate_parameters(int *vertices_count, int *edges_count);
 bool validate_edge(int src, int dst, int vertices_count);
-bool graph_scan_validate_edges(Graph *graph);
+bool scan_validate_edges(Graph *graph);
 
 #endif  // INPUT_H
