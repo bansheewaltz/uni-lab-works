6c7ae46c79490f6664aee8eceb10e7488b74fdf6
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-24 10:42:42 +0700

    test_runner.sh: asan uncommented

--- lab8-1/test/scripts/test_runner.sh
+++ lab8-1/test/scripts/test_runner.sh
@@ -6,21 +6,21 @@ mkdir build
 cd build
 
 
-cmake .. -GNinja -DUNLIMITED=ON -DCMAKE_C_COMPILER=clang
+cmake .. -GNinja -DUNLIMITED=ON -DCMAKE_C_COMPILER=gcc
 ninja
-ninja test || STATUS=1
+ninja test  || STATUS=1
 
 rm -rf *
 
-# cmake .. -GNinja -DUNLIMITED=ON -DCMAKE_C_COMPILER=gcc -DENABLE_ASAN=true
-# ninja
-# ninja test || STATUS=3
-
-# # rm -rf *
-
-cmake .. -GNinja -DUNLIMITED=ON -DCMAKE_C_COMPILER=gcc -DENABLE_USAN=true
+cmake .. -GNinja -DUNLIMITED=ON -DCMAKE_C_COMPILER=clang -DENABLE_ASAN=true
 ninja
-ninja test || STATUS=4
+ninja test  || STATUS=3
+
+rm -rf *
+
+cmake .. -GNinja -DUNLIMITED=ON -DCMAKE_C_COMPILER=clang -DENABLE_USAN=true
+ninja
+ninja test  || STATUS=4
 
 ninja check || STATUS=5
 
