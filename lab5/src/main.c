628b8173b95c7d3a07bb019b2ee029c82be0afe7
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-29 15:29:36 +0700

    main.c: redundant includes removed

--- lab5/src/main.c
+++ lab5/src/main.c
@@ -1,14 +1,7 @@
 #include "main.h"
 
 #include <assert.h>
-#include <errno.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "encoding.h"
 #include "input.h"
