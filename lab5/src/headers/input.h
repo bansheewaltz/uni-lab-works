4d38844669d16bb48c1c03057d28721e2a7bf726
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-05-29 21:53:04 +0700

    encoding works

--- lab5/src/headers/input.h
+++ lab5/src/headers/input.h
@@ -6,8 +6,8 @@
 
 #include "typedefs.h"
 
-void scan_chars_frequencies_from_file(CharInfo **chars_info, FILE *input);
-int read_alphabet_size(FILE *input);
+void scan_chars_frequencies_from_input(CharInfo **dictionary, FILE *input);
+int read_alphabet_size_checked(FILE *input);
 size_t get_alphabet_size(CharInfo **chars_info);
 
 #endif  // INPUT_H_
--- lab5/src/input.c
+++ lab5/src/input.c
@@ -1,43 +1,55 @@
 #include "input.h"
 
+#include <assert.h>
+#include <stdlib.h>
+
 #include "main.h"
 #include "utils.h"
 
-void scan_chars_frequencies_from_file(CharInfo **chars_info, FILE *input)
+void scan_chars_frequencies_from_input(CharInfo **dictionary, FILE *input)
 {
-  uchar buffer[BUFFER_SIZE];
-  size_t chars_read;
+  // initialize info for every char in an alphabet to minimize number of
+  // actions during iteration over input
+  for (int i = 0; i < CHARSET_SIZE; ++i) {
+    dictionary[i] = create_and_initialize_char_info(i);
+  }
 
+  uchar buffer[BUFFER_SIZE];
+
+  size_t chars_read = 0;
   while ((chars_read = fread(buffer, sizeof(uchar), BUFFER_SIZE, input)) > 0) {
     for (size_t i = 0; i < chars_read; ++i) {
-      if (chars_info[buffer[i]] == NULL) {
-        chars_info[buffer[i]] = create_and_initialize_char_info(buffer[i]);
-      }
-      ++chars_info[buffer[i]]->freq;
+      ++dictionary[buffer[i]]->freq;
+    }
+  }
+
+  // free info for chars that are never encountered
+  for (int i = 0; i < CHARSET_SIZE; ++i) {
+    if (dictionary[i]->freq == 0) {
+      free(dictionary[i]);
+      dictionary[i] = NULL;
     }
   }
 }
 
-int read_alphabet_size(FILE *input)
+int read_alphabet_size_checked(FILE *input)
 {
-  byte alph_size;
-  fread(&alph_size, sizeof(uchar), 1, input);
-
+  byte alph_size = 0;
+  size_t rc = fread(&alph_size, sizeof(uchar), 1, input);
+  assert(rc == 1);
   if (alph_size == 0b00000000) {
     return EIGHT_BIT_CHARACTER_SET_SIZE;
   }
   return (int)alph_size;
 }
 
-size_t get_alphabet_size(CharInfo **chars_info)
+size_t get_alphabet_size(CharInfo **dictionary)
 {
   size_t alph_size = 0;
-
   for (size_t i = 0; i < CHARSET_SIZE; ++i) {
-    if (chars_info[i] != NULL) {
+    if (dictionary[i] != NULL) {
       ++alph_size;
     }
   }
-
   return alph_size;
 }
