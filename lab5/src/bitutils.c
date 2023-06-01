3c90e7cef6a880fd0fd8b87c53e2e8b73061accd
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-06-01 13:08:23 +0700

    bitutils.c: bit-ascii transition functions added

--- lab5/src/bitutils.c
+++ lab5/src/bitutils.c
@@ -1,23 +1,35 @@
 #include "bitutils.h"
 
+#include <assert.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "main.h"
 #include "output.h"
+#include "typedefs.h"
 
 uchar bit_to_char(int bit)
 {
-  if (bit == 0) {
+  if (bit == 0)
     return '0';
-  }
-  return '1';
+  if (bit == 1)
+    return '1';
+  assert(false);
+}
+
+bool char_to_bit(int bit_ascii)
+{
+  if (bit_ascii == '0')
+    return 0;
+  if (bit_ascii == '1')
+    return 1;
+  assert(false);
 }
 
 void set_bit(uchar *byte, bool bit, int position)
 {
-  *byte |= bit << (CHAR_BIT - position);
+  *byte |= (uint)bit << (CHAR_BIT - (uint)position);
 }
 
 void reset_byte(uchar *byte, int *bit_position)
@@ -29,6 +41,7 @@ void reset_byte(uchar *byte, int *bit_position)
 bool get_bit(uchar byte, int position)
 {
   return (byte >> (CHAR_BIT - position)) & 0b00000001;
+  // return ((uint)byte >> (CHAR_BIT - (uint)position)) & 0x0001U;
 }
 
 bool readbit_buffered(FILE *input)
@@ -105,4 +118,35 @@ void writebit_buffered(int bit, bool flush_flag, FILE *output)
       ++bit_position;
     }
   }
+}
+
+uint64_t read_bits_ascii(FILE *input, size_t bytes_count)
+{
+  assert(bytes_count <= sizeof(int64_t));
+
+  uint64_t value = 0x0U;
+  uint64_t power_of_2 = 1;
+  size_t bits_count = bytes_count * CHAR_BIT;
+
+  uchar *buffer = malloc(bits_count);
+  size_t rc = fread(buffer, sizeof(uint8_t), bits_count, input);
+  assert(rc == bits_count);
+
+  for (size_t i = bits_count - 1; i < bits_count; --i) {
+    bool bit = char_to_bit(buffer[i]);
+    if (bit == 1) {
+      value += power_of_2;
+    }
+    power_of_2 *= 2;
+  }
+
+  free(buffer);
+  return value;
+}
+
+bool readbit_ascii(FILE *input)
+{
+  int bit_ascii = getc(input);
+  assert(bit_ascii != -1);
+  return char_to_bit(bit_ascii);
 }
